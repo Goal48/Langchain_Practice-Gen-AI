26d2038da745ca9fe84486f67f57c54073b6b31f
Pius Dutta <piusduttanitdgpbt@gmail.com>
2026-06-25 16:48:08 +0530

    structure output

---
+++ Output/OutputinTypedict.py
@@ -0,0 +1,22 @@
+import os
+from typing import TypedDict,Annotated,Optional
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+load_dotenv()
+
+class Review(TypedDict):
+    sentiment: Annotated[Optional[str],"Return the sentiment"]
+    rating: Annotated[int,"Return the rating"]
+    summary: Annotated[str,"Return the summary"]
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+# 3. Bind the structured output
+structured_llm = model.with_structured_output(Review)
+
+# 4. Invoke it
+response = structured_llm.invoke("The product arrived on time and works beautifully! 5 stars.")
+print(response)
+
